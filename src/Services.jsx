ee4bf7c98f2c80616d70f2384001ca91d59e29f8
Sania More <saniamore205@gmail.com>
2026-03-23 22:34:51 +0530

    small addition off animation

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Packages from './Packages';
+import { easeIn, motion } from 'framer-motion';
 
 const Services = () => {
   
@@ -35,19 +36,39 @@ const Services = () => {
         description: "Get styling tips and pose ideas for the perfect travel photos."
     }
     ];
+
+    const containerVariant = {
+        hidden: {},
+        show: {
+            transition: {staggerChildren: 0.2}
+        }
+    }
     
+    const cardVariant = {
+        hidden: {y: 80, opacity: 0},
+        show: {
+            y: 0, opacity: 1,
+            transition: {type: "spring", stiffness: 300, damping: 20, ease: "easeIn"}
+
+        }
+    }
+
   return (
     <div>
         <div className='section'>
         <h1>Our Services</h1>
-        <div className='services-grid'>
-            {Ourservice.map((service) => (
-                <div className='ser-card' key={service.id}>
-                <h2>{service.title}</h2>
-                <p>{service.description}</p>
-                </div>
-            ))}
-        </div>
+            <motion.div className="services-grid"
+                variants={containerVariant}
+                initial="hidden"
+                whileInView="show"
+                viewport={{ once: false , margin: "-100px"}}>
+                {Ourservice.map((service) => (
+                    <motion.div variants={cardVariant} className='ser-card' key={service.id}>
+                    <h2>{service.title}</h2>
+                    <p>{service.description}</p>
+                    </motion.div>
+                ))}
+            </motion.div>
         </div>
         <Packages/>
     </div>
